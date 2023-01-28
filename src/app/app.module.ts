962e0f01c1457ba36adeec8052c9840af53462ae
Abdelrahman Kamal Nasr <aknasr@ejada.com>
2023-01-28 23:07:59 +0200

    adding Task interface and Tmp tasks array to test functionality with and the skeleton for task list component yet to be implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ButtonComponent } from './components/generic/button/button.component';
 import { HeaderComponent } from './components/header/header.component';
+import { TaskListComponent } from './components/task-list/task-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ButtonComponent,
-    HeaderComponent
+    HeaderComponent,
+    TaskListComponent
   ],
   imports: [
     BrowserModule
