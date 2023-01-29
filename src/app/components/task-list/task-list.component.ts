0e0918d395fd5767a664ed337a0e9544598e4988
Abdelrahman Kamal Nasr <aknasr@ejada.com>
2023-01-29 11:01:18 +0200

    init the task-list

--- src/app/components/task-list/task-list.component.ts
+++ src/app/components/task-list/task-list.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
-
+import {TASKS} from '../../tmp-tasks';
+import {Task} from '../../Interfaces/Task';
 @Component({
   selector: 'app-task-list',
   templateUrl: './task-list.component.html',
   styleUrls: ['./task-list.component.css']
 })
 export class TaskListComponent {
+  tasks : Task[] = TASKS;
 
+  stringify(task :Task) : string{
+    return JSON.stringify(task);
+  }
 }
