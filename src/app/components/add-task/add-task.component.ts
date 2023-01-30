f7ad8b4a0a3cd47bd24630707066978556d997cb
Abdelrahman Kamal Nasr <aknasr@ejada.com>
2023-01-30 11:59:55 +0200

    adding simple validation to the add form

--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -11,24 +11,47 @@ import { AddTaskTogglerService } from 'src/app/services/add-task-toggler.service
 export class AddTaskComponent {
   show : boolean = false;
   subscription! : Subscription;
+  addForm! : FormGroup;
 
   constructor(private togglerService : AddTaskTogglerService){
     this.subscription = togglerService.toggler().subscribe(
       (nextVal) => {this.show = nextVal;}
     );
   }
-  addForm = new FormGroup({
-    text: new FormControl('', Validators.required),
-    date: new FormControl('' ),
-    reminder: new FormControl()
-  });
+  
+
+  ngOnInit(){
+    this.addForm = new FormGroup({
+      text: new FormControl('', [Validators.required]),
+      date: new FormControl('' ),
+      reminder: new FormControl()
+    });
+  }
 
   onSubmit(){
+    if(this.addForm.invalid){
+      this.alertErrors();
+      return;
+    }
     //console.log(this.addForm.value);
     //TODO actually add the task
+    this.addForm.reset();
+  }
+
+  alertErrors(){
+    //console.log(this.addForm)
+    let errors : string = "";
+    for(let formcontrol in this.addForm.controls)
+      for(let error in this.addForm.controls[formcontrol].errors)
+        errors += error.toString();
+      alert(errors);
   }
 
   OnDestroy(){
     this.subscription.unsubscribe();
   }
+
+  get text() { return this.addForm.get('text')!; }
+
+
 }
