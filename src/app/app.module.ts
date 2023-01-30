35cf6b0c044bb1d24a1cc22697fa05bc2f8abb5b
Abdelrahman Kamal Nasr <aknasr@ejada.com>
2023-01-31 00:11:43 +0200

    adding simple navbar and footer yet to handle the login log out toggling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,6 +24,7 @@ const routes : Routes = [
   {path:'about' , component: AboutComponent},
   {path:'help' , component: HelpComponent},
   {path:'login' , component: LoginComponent},
+  {path:'logout' , component: LoginComponent}
 ];
 
 @NgModule({
