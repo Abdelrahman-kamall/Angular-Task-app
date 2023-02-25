1eabe8775b3d13fab1c21c61f2ddc0c99bacf02d
Abdelrahman Kamal Nasr <aknasr@ejada.com>
2023-02-25 14:48:08 +0200

    finishing the task service

--- src/app/Interfaces/Task.ts
+++ src/app/Interfaces/Task.ts
@@ -1,6 +1,6 @@
 export interface Task {
     id?: number;
     text: string;
-    date: string;
+    date?: string;
     reminder: boolean;
 }
--- src/app/services/tasks.service.ts
+++ src/app/services/tasks.service.ts
@@ -1,18 +1,37 @@
-import { Injectable } from '@angular/core';
-import {HttpClient, HttpHeaders} from "@angular/common/http"
+;import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders, HttpParamsOptions} from "@angular/common/http"
 import { Observable } from 'rxjs';
 import { Task } from '../Interfaces/Task';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json',
+  })  };
 
 @Injectable({
   providedIn: 'root'
 })
 export class TasksService {
 
-  private baseURL : string = "http://localhost:3000/tasks";
+  private baseURL : string = "http://localhost:3000/tasks/"
 
   constructor(private httpClient : HttpClient) { }
 
   getTasks(): Observable<Task[]>{
     return this.httpClient.get<Task[]>(this.baseURL);
+  }
+
+  postTask(task: Task): Observable<Task>{
+    return this.httpClient.post<Task>(this.baseURL,task,httpOptions);
+  }
+
+  putTask(id: string, task: Task): Observable<Task>{
+    const url = `${this.baseURL}/${task.id}`;
+    return this.httpClient.put<Task>(url,task,httpOptions);
+  }
+
+  deleteTask(task: Task): Observable<Task>{
+    const url = `${this.baseURL}/${task.id}`;
+    return this.httpClient.delete<Task>(url);
   }
 }
